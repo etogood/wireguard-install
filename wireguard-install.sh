dfbaa9f11e7e7dcace14acf74ad5977592f055c7
etogood <ilya.etogood@yandex.ru>
2023-10-25 16:36:44 +0300

    1

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -308,8 +308,7 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
 			echo "http://dl-cdn.alpinelinux.org/alpine/v3.11/community" >> /etc/apk/repositories
 			apk update
 			apk add ip6tables iptables
-			apk add -u wireguard-tools wireguard-virt libqrencode $firewall
-			apk add -u wireguard-tools wireguard-lts libqrencode $firewall
+			apk add -u wireguard-tools wireguard libqrencode $firewall
 		elif [[ "$os" == "ubuntu" ]]; then
 			# Ubuntu
 			apt-get update
